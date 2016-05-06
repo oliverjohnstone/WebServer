abde739721f82455ae5ad2234740ecdd45378bb4
Oliver Johnstone <oliver.johnstone@clock.co.uk>
2016-05-06 22:28:20 +0100

    Removes sig handler

--- main.cpp
+++ main.cpp
@@ -1,14 +1,10 @@
 #include <iostream>
-#include <signal.h>
-#include <stdlib.h>
 #include "src/Config/Config.h"
 #include "src/Config/ProgramOptions.h"
 #include "src/Logger/Logger.h"
 #include "src/Server/Server.h"
 
-void setupCancelSigHandler();
-
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
     Logger logger;
     int exitCode = 0;
 
@@ -21,7 +17,6 @@ int main(int argc, char** argv) {
         } else {
             Config config(options.getConfigPath());
             Server server(config, logger);
-            setupCancelSigHandler();
             server.start();
         }
     } catch (std::exception &e) {
@@ -32,18 +27,4 @@ int main(int argc, char** argv) {
     logger.info("Exiting");
     logger.flush();
     return exitCode;
-}
-
-void sigHandler(int signal) {
-    cout << signal << endl;
-}
-
-void setupCancelSigHandler() {
-    signal(SIGINT, &sigHandler);
-//    struct sigaction sigIntHandler;
-//    sigIntHandler.sa_handler = sigHandler;
-//    sigemptyset(&sigIntHandler.sa_mask);
-//    sigIntHandler.sa_flags = 0;
-//
-//    sigaction(SIGINT, &sigIntHandler, NULL);
 }
